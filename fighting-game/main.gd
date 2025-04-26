e5235a080358b04955ccbbf30f50ca9ccb6b10eb
aroll6 <157648685+aroll6@users.noreply.github.com>
2025-04-26 13:47:21 -0400

    Version 0.0.02

--- fighting-game/main.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-var players
-var player1
-var player2
-var fighter = preload("res://fighter.tscn")
-func _ready():
-	pass
-	
--- fighting-game/main_menu.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-
-var player = preload("res://fighter.tscn")
-func _on_button_button_down():
-	get_tree().change_scene_to_file("res://stage.tscn")
-	
